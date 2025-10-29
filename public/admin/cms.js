d74111c35ef89af4b7d5a818bfae07e4f0218472
Gary Pope <gary.pope@epicompany.eu>
2025-10-29 21:55:32 +0000

    Fix admin

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,23 +1,25 @@
 // CMS is available globally from the CDN
-CMS.registerPreviewTemplate("acts", ({ entry, getAsset }) => {
-  const data = entry.getIn(["data"]).toJS();
-  const image = getAsset(data.image);
 
-  return `
-    <div style="border:1px solid #ccc; padding:16px; width:300px;">
-      ${image ? `<img src="${image}" style="width:100%; border-radius:8px;" />` : ""}
-      <h2>${data.title}</h2>
-      <p>${data.description}</p>
-      ${
-        data.categories
-          ? data.categories.map(cat => `<span>${cat}</span>`).join(" ")
-          : ""
-      }
-      ${
-        data.keywords
-          ? data.keywords.map(tag => `<small>#${tag}</small>`).join(" ")
-          : ""
-      }
-    </div>
-  `;
-});
+// Uncomment when we're ready to work on preview templates
+//CMS.registerPreviewTemplate("acts", ({ entry, getAsset }) => {
+//  const data = entry.getIn(["data"]).toJS();
+//  const image = getAsset(data.image);
+//
+//  return `
+//    <div style="border:1px solid #ccc; padding:16px; width:300px;">
+//      ${image ? `<img src="${image}" style="width:100%; border-radius:8px;" />` : ""}
+//      <h2>${data.title}</h2>
+//      <p>${data.description}</p>
+//      ${
+//        data.categories
+//          ? data.categories.map(cat => `<span>${cat}</span>`).join(" ")
+//          : ""
+//      }
+//      ${
+//        data.keywords
+//          ? data.keywords.map(tag => `<small>#${tag}</small>`).join(" ")
+//          : ""
+//      }
+//    </div>
+//  `;
+//});
